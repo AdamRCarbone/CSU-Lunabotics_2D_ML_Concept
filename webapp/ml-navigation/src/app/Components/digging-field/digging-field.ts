b8f580983cc19be569b428ad29162077825c6ebf
Adam Carbone <adam.r.carbone@live.com>
2025-11-04 22:00:03 -0500

    generated digging-field component

---
+++ webapp/ml-navigation/src/app/Components/digging-field/digging-field.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-digging-field',
+  imports: [],
+  templateUrl: './digging-field.html',
+  styleUrl: './digging-field.css',
+})
+export class DiggingField {
+
+}
