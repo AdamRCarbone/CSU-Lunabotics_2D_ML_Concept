e34c9d547a7cb666ef2db65d96054a8fe8f9db61
SkocicD <skocicdavid@gmail.com>
2025-11-11 23:27:41 -0500

    deleted all files :)

--- webapp/ml-navigation/src/app/enums/zone.enum.ts
+++
@@ -1,9 +0,0 @@
-// Zone types in environment
-export enum Zone {
-  STARTING = 'starting',
-  EXCAVATION = 'excavation',
-  OBSTACLE = 'obstacle',
-  CONSTRUCTION = 'construction',
-  TARGET_BERM = 'target_berm',
-  NONE = 'none' // No specific zone
-}
