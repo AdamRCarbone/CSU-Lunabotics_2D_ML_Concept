288557ade876cc1305392edb192ac1e618311b3b
SkocicD <skocicdavid@gmail.com>
2025-11-16 14:56:31 -0500

    tank drive and acceleration added, as well as using meters and scaling to pixels

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-import threading
 import time
-from shapes import Rectangle, Circle
-from MapObjects import Zone, Obstacle
+from shapes import Rectangle, Circle, Zone, Obstacle, Rover
 import tkinter as tk
 
 
+keys_pressed = []
+
+
 def on_key_press(event):
-    global c
-    c.move(5, 0)
+    global keys_pressed
+    keys_pressed.append(event.keysym)
+
+
+def on_key_release(event):
+    global keys_pressed
+    keys_pressed.remove(event.keysym)
 
 
 def worker():
@@ -19,35 +25,54 @@ def worker():
         time.sleep(.1)
 
 
-def screenUpdate():
-    global arena
+def loop():
+    global keys_pressed
     global root
+    global bot
 
-    root.after(20, screenUpdate)
+    # update the speed of Rover
+    # update the position of Rover
+    # check collision of Rover with all other objects
+
+    for key in keys_pressed:
+        match(key):
+            case('a'):
+                bot.torque_right(-5)
+            case('q'):
+                bot.torque_right(5)
+            case('d'):
+                bot.torque_left(5)
+            case('e'):
+                bot.torque_left(-5)
+
+    bot.update()
+    root.after(20, loop)
 
 
 root = tk.Tk()
 root.title("ML Map")
-canvas = tk.Canvas(root, width=600, height=600, bg="white")
+canvas = tk.Canvas(root, width=688, height=500, bg="white")
 canvas.pack()
 
-r = Rectangle(canvas, 600, 600, 300, 300, color='blue')
-arena = Zone(r)
-r = Rectangle(canvas, 300, 600, 150, 300, color='green')
-digging_zone = Zone(r)
-r = Rectangle(canvas, 250, 100, 150, 500, color='red')
-starting_zone = Zone(r)
-c = Circle(canvas, 25, 150, 500, color='purple')
-obstacle = Obstacle(c)
+arena = Zone(canvas, (0, 5), (6.88, 0), 'blue')
+excavation_zone = Zone(canvas, (0, 5), (2.5, 0), 'green')
+obstacle_zone = Zone(canvas, (2.5, 5), (6.88, 0), 'red')
+start_zone = Zone(canvas, (0, 2), (2, 0), 'blue')
+# c = Circle(canvas, 25, 150, 500, color='purple')
+print('bot vvv')
+bot = Rover(canvas)
 
-arena.add(digging_zone)
-arena.add(starting_zone)
-arena.add(obstacle)
+arena.add(excavation_zone)
+arena.add(obstacle_zone)
+arena.add(start_zone)
+arena.add(bot)
 
 # thread = threading.Thread(target=worker)
 # thread.start()
 # screenUpdate()
 
 root.bind('<KeyPress>', on_key_press)
+root.bind('<KeyRelease>', on_key_release)
+loop()
 root.mainloop()
 # thread.join()
--- shapes.py
+++ shapes.py
@@ -1,55 +1,32 @@
+from __future__ import annotations
 from abc import ABC, abstractmethod
-import threading
 import math
-import tkinter as tk
 
 
 class Shape(ABC):
+    scale = 100
+
     def __init__(self, canvas, color, outline_color):
         self.color = color
         self.outline_color = outline_color
         self._canvas = canvas
-        self._lock: threading.Lock = threading.Lock()
 
-    @abstractmethod
-    def move(self):
-        pass
+    def arena_to_canvas(self, x, y):
+        return (self.scale * x, self.scale * (5-y))
 
-
-class Polygon(Shape):
-    def __init__(self, canvas, points, color='white', outline_color='black'):
-        super().__init__(canvas, color, outline_color)
-        self._points = points
-
-        self._obj = self._canvas.create_polygon(
-            [coord for point in self._points for coord in point],
-            fill=self.color,
-            outline=self.outline_color,
-            width=3
-        )
-
-    def set_points(self):
-        points_1d = [coord for point in self._points for coord in point]
-        self._canvas.coords(
-            self._obj,
-            *points_1d
-        )
-
-    def move(self, dx, dy):
-        for i, point in enumerate(self._points):
-            self._points[i] = (point + dx, point+dy)
-        self.set_points()
+    def meters_to_pixels(self, val):
+        return self.scale * val
 
 
-class Rectangle(Polygon):
+class Rectangle(Shape):
     '''
     A class for a rectangle centered at point (x, y)
     the rectangle can be rotated by any amount
     '''
 
-    def __init__(self, canvas, width, height, x, y, angle=0, color='white', outline_color='black'):
-        # call polygon constructor with no points (update them later)
-        super().__init__(canvas, [(0, 0)]*4, color, outline_color)
+    def __init__(self, canvas, width, height, x, y, angle=0, color='white', outline_color='black', **kwargs):
+        super().__init__(canvas, color, outline_color)
+        print('here')
 
         # set class variables
         self._width = width
@@ -58,14 +35,15 @@ class Rectangle(Polygon):
         self._y = y
         self._angle = angle
 
-        # update the points in the polygon
-        self.update_points()
+        self._obj = self._canvas.create_polygon(
+            [0]*8,
+            fill=self.color,
+            outline=self.outline_color,
+            width=3
+        )
+        self.calculate_corners()
 
-    def update_points(self):
-        self._lock.acquire()
-
-        # since all corners are the same distance from the center
-        # get the magnitude of x and y from the center first
+    def calculate_corners(self):
         unrotated = [
             (-self._width/2, -self._height/2),
             (self._width/2, -self._height/2),
@@ -77,28 +55,39 @@ class Rectangle(Polygon):
 
         # apply rotation matrix
         rotated = [(x*c+y*s, -x*s+y*c) for x, y in unrotated]
-        self._points = [(x+self._x, y+self._y) for x, y in rotated]
+        self.corners = [(x+self._x, y+self._y) for x, y in rotated]
+        _corners = [self.arena_to_canvas(x, y) for x, y in self.corners]
 
-        self._lock.release()
-        self.set_points()
+        points_1d = [coord for point in _corners for coord in point]
+        self._canvas.coords(
+            self._obj,
+            *points_1d
+        )
 
-    def rotate(self, da):
-        self._lock.acquire()
+    def set_pos(self, x=None, y=None, angle=None):
+        if x:
+            self._x = x
+        if y:
+            self._y = y
+        if angle:
+            self._angle = angle
+        self.calculate_corners()
+
+    def rotate_about(self, x, y, da):
+        # move s and y to have the point as the origin
+        tmp_x = self._x - x
+        tmp_y = self._y - y
+
+        # Apply rotation matrix
+        c = math.cos(math.radians(da))
+        s = math.sin(math.radians(da))
+        rot_x = tmp_x * c + tmp_y * s
+        rot_y = -tmp_x * s + tmp_y * c
+
+        # Shift the coordinates back and update new angle
+        self._x = rot_x + x
+        self._y = rot_y + y
         self._angle += da
-        self._lock.release()
-        self.update_points()
-
-    def set_x(self, x):
-        self.move(x-self._x)
-        self._lock.acquire()
-        self._x = x
-        self._lock.release()
-
-    def set_y(self, y):
-        self.move(y-self._y)
-        self._lock.acquire()
-        self._y = y
-        self._lock.release()
 
 
 class Circle(Shape):
@@ -133,3 +122,120 @@ class Circle(Shape):
         self._lock.acquire()
         self._canvas.move(self._obj, dx, dy)
         self._lock.release()
+
+
+class PhysicsObject(ABC):
+    def __init__(self, mass, **kwargs):
+        super().__init__(**kwargs)
+        self._mass = mass
+        self._vx = 0
+        self._vy = 0
+
+    def force(self, mag, dir):
+        fx = mag * math.cos(math.radians(dir))
+        fy = mag * math.sin(math.radians(dir))
+
+        self._vx += fx / self._mass
+        self._vy += fy / self._mass
+        if (r := abs(self._vx/20)) > 1:
+            self._vx /= r
+        if (r := abs(self._vy/20)) > 1:
+            self._vy /= r
+
+
+class PhysicsRectangle(PhysicsObject, Rectangle):
+    def __init__(self, canvas, width, height, x, y, color, outline_color, mass):
+        super().__init__(
+            mass=mass,
+            canvas=canvas,
+            width=width,
+            height=height,
+            x=x,
+            y=y,
+            color=color,
+            outline_color=outline_color
+        )
+
+        self._I_CM = mass * \
+            (self._width/12 + self._height/12)
+        # Apply parallel axis theorem
+        self._I_Corner = self._I_CM + mass * \
+            ((self._width/2)**2 + (self._height/2)**2)
+
+        self._I_Edge = self._I_CM + mass * (self._width/2)**2
+
+        self._angle = 0
+
+        # tank drive means that the bot will rotate around the left side
+        # when the right is driving and vice versa, so make vars for each
+        self.omega_right = 0
+        self.omega_left = 0
+
+    def torque_right(self, t):
+        self.omega_right += t / self._I_Edge
+
+    def torque_left(self, t):
+        self.omega_left += t / self._I_Edge
+
+    def drag(self):
+        self.omega_right -= self.omega_right * .2
+        self.omega_left -= self.omega_left * .2
+
+    def update(self):
+        self.drag()
+        tl, tr, br, bl = self.corners
+        right_midpoint = ((tr[0]+br[0])/2, (tr[1]+br[1])/2)
+        left_midpoint = ((tl[0]+bl[0])/2, (tl[1]+bl[1])/2)
+        self.rotate_about(*right_midpoint, self.omega_right)
+        self.rotate_about(*left_midpoint, self.omega_left)
+        self._x += self._vx
+        self._y += self._vy
+        self.calculate_corners()
+
+
+class MapObject(ABC):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.children: list[MapObject] = []
+
+    def add(self, other: MapObject):
+        self.children.append(other)
+
+
+class Obstacle(MapObject):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+class Rover(PhysicsRectangle):
+    def __init__(self, canvas):
+        super().__init__(canvas, .75, 1.5, 1, 1, 'black', 'black', mass=80)
+
+    def drive_left(self, magnitude):
+        if magnitude > 0:
+            self.torque_right(magnitude)
+        else:
+            self.torque(magnitude, 0)
+        # self.force(5, self._angle + 90)
+
+    def drive_right(self, magnitude):
+        if magnitude > 0:
+            self.torque(magnitude, 1)
+        else:
+            self.torque(magnitude, 2)
+        # self.force(5, self._angle + 90)
+
+
+class Zone(MapObject, Rectangle):
+    def __init__(self, canvas, top_left, bottom_right, color):
+        width = bottom_right[0]-top_left[0]
+        height = top_left[1]-bottom_right[1]
+        print(width, height)
+        super().__init__(
+            canvas=canvas,
+            width=width,
+            height=height,
+            x=top_left[0] + width/2,
+            y=bottom_right[1] + height/2,
+            color=color
+        )
